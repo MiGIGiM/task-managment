e7e7f1d27ccfa8740b7cb4d46e4b887b3933118f
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-11-16 20:40:41 -0600

    fix: removing unused code

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { Squares2X2Icon, Bars3Icon } from '@heroicons/react/24/outline'
 import PlusButton from '../components/PlusButton'
 import Layout from '../layout'
 import Toggle from '../components/Toggle'
@@ -11,10 +10,6 @@ const Dashboard = () => {
     <Layout>
       <section>
         <div className='flex justify-between'>
-          {/* <span className='flex items-center space-x-3'>
-          <Squares2X2Icon className='h-5 w-5 stroke-2  group-first/active:text-primary-4 group-hover:text-primary-4' />
-          <Bars3Icon className='h-5 w-5 stroke-2 group-first/active:text-primary-4 group-hover:text-primary-4' />
-        </span> */}
           <Toggle selected={sel} setSelected={setSel} />
           <PlusButton />
         </div>
